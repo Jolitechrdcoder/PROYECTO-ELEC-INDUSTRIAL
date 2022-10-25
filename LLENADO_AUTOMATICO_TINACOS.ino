66c729a2f81d59bfa117c9f1d06fb22c9062233f
Julio de la Rosa Pèrez <113809728+Juliodelarosap@users.noreply.github.com>
2022-10-25 13:38:41 -0400

    Update LLENADO_AUTOMATICO_TINACOS.ino

--- LLENADO_AUTOMATICO_TINACOS.ino
+++ LLENADO_AUTOMATICO_TINACOS.ino
@@ -5,7 +5,7 @@ int TRIG = 10;			// trigger en pin 10
 int ECO = 9;			// echo en pin 9
 int LED = 3;			// LED en pin 3
 int DURACION;
-int DISTANCIA;
+int DISTANCIA;    // hola jorge es julio xdxd
 int VERDE = 4;
 int NARANJA = 5;
 int ROJO= 6;
