9af42ddcff81eea155cf563c5129d809af0baf97
Jolitechrdcoder <114448996+Jolitechrdcoder@users.noreply.github.com>
2022-11-30 21:36:55 -0400

    Update codigo_2_maquina_llenado_de_botellas.ino

--- codigo_2_maquina_llenado_de_botellas.ino
+++ codigo_2_maquina_llenado_de_botellas.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(34, 16, 2);
 int sensor1 = 2;//sensor 1
 int cinta = 3;//rele que enciende cinta
 int bomba =4;//rele2 
@@ -16,6 +16,9 @@ void setup()
   pinMode(sensor1,INPUT);
   pinMode(cinta,OUTPUT);
   pinMode(bomba,OUTPUT);
+  lcd.setCursor(0, 1);
+    lcd.print("Botellas:");
+  
   
 }
 //Algoritmo maquina llenado
@@ -24,8 +27,10 @@ void loop()
   estado = digitalRead(sensor1);//Almacena lectura del sensor 
  
   
-  if(estado == 0){//Condicional cinta transportadora y llenado.
+  if(estado == 1){//Condicional cinta transportadora y llenado.
   cont++;
+    lcd.setCursor(10, 1);
+   lcd.print(cont);
    Serial.println(cont);
   digitalWrite(cinta,1);//DETENGO MOTOR CINTA
   delay(500);
@@ -33,12 +38,15 @@ void loop()
   delay(500);
   digitalWrite(cinta,1);
     digitalWrite(bomba,1);//enciendo bomba
+    lcd.setCursor(0, 0);
+    lcd.print("  !!LLENANDO!!");
     Serial.println("!!LLENANDO!!");
     delay(6000);//tiempo de llenado
    
   }
   else{
-    
+    lcd.setCursor(0, 0);
+    lcd.print("               ");
     digitalWrite(cinta,0);
     digitalWrite(bomba,0);
     
